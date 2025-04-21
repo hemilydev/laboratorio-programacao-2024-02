18318a5d070c18a3963925b5cb3fd1618f2ccd20
Hemily <hemilydev@gmail.com>
2025-04-21 14:50:33 -0300

    Main.java

---
+++ PBL01/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        String nome;
+        nome = "Hemily";
+        
+        int idade;
+        idade = 28;
+        
+        System.out.println("Hello World");
+        System.out.println("Meu nome é " + nome);
+        System.out.println("Estou no primeiro módulo do curso de ADS.");
+        System.out.println("Moro em Goiânia, tenho " + idade + " anos e já sou formada em Ciências Contábeis.");
+        System.out.println("Optei por fazer esse curso, para ajudar na área contábil.");
+        System.out.println("Nesse pouco tempo de aula, já estou gostando muito e vejo um futuro promissor.");
+    }
+}
