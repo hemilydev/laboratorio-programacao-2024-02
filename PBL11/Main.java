72d072461a2a34484995b57188e34d9ebc911116
Hemily <hemilydev@gmail.com>
2025-04-22 11:56:38 -0300

    Main.java

---
+++ PBL11/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        System.out.print("Quantos nomes você deseja declarar? ");
+        int count = ler.nextInt();
+        ler.nextLine();
+
+        String[] nomes = new String[count];
+        int[] idades = new int[count];
+
+        for (int i = 0; i < count; i++) {
+            System.out.print("Qual o nome da pessoa " + (i + 1) + ": ");
+            nomes[i] = ler.nextLine();
+            System.out.print("Qual a idade da pessoa " + (i + 1) + ": ");
+            idades[i] = ler.nextInt();
+            ler.nextLine();
+        }
+
+        System.out.println("\nNomes e idades declarados:");
+        for (int i = 0; i < count; i++) {
+            System.out.println("Nome " + (i + 1) + ": " + nomes[i] + ", Idade: " + idades[i]);
+        }
+
+        ler.close();
+    }
+}
